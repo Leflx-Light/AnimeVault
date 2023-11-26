6ce422fbc93fd7fe79261d246a86c63491235eb8
Leflx-Light <leflxlight@gmail.com>
2023-11-26 21:41:29 +0530

    First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={dmSans.className}>
-        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+        <main className="max-w-7xl mx-auto bg-[#1c202e] px-[10em]">
           <Hero />
           {children}
           <Footer />
